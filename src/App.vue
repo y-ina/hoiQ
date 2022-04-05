428606450d6390f8af3df922ef6d168ca8a73250
YUTA INAGAWA <obanndesu11241988@yahoo.co.jp>
2022-04-05 22:45:35 +0900

    サイドメニュー共通化

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,42 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-sheet
+        color="grey lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div class="username">john@vuetifyjs.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text, to] in links"
+          :key="icon"
+          :to="to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <router-view/>
   </div>
 </template>
@@ -29,4 +65,32 @@
     }
   }
 }
+
+.grey.darken-1 {
+  background-color: #757575;
+  border-color: #757575;
+}
+
+.pa-4 {
+  padding: 16px;
+}
+
+.username {
+  padding-top: 10px;
+}
+
 </style>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      links: [
+        ['mdi-inbox-arrow-down', 'Inbox', '/'],
+        ['mdi-send', 'Send', '/about'],
+        ['mdi-delete', 'Trash', '/about'],
+        ['mdi-alert-octagon', 'Spam', '/about'],
+      ],
+    }),
+  }
+</script>
